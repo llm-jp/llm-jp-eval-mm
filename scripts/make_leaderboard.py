630df7b30a752319f096981b633f447a8b96873b
speed1313 <speedtry13@icloud.com>
2025-03-19 12:43:17 +0900

    Format

--- scripts/make_leaderboard.py
+++ scripts/make_leaderboard.py
@@ -6,50 +6,116 @@ from typing import Dict, List, Optional
 
 BENCHMARK_METRICS = {
     "japanese-heron-bench": [
-        "conv", "detail", "complex", "overall",
-        "conv_rel", "detail_rel", "complex_rel", "overall_rel",
+        "conv",
+        "detail",
+        "complex",
+        "overall",
+        "conv_rel",
+        "detail_rel",
+        "complex_rel",
+        "overall_rel",
     ],
     "ja-vlm-bench-in-the-wild": [
-        "rougel", "llm_as_a_judge",
+        "rougel",
+        "llm_as_a_judge",
     ],
     "ja-vg-vqa-500": [
-        "rougel", "llm_as_a_judge",
+        "rougel",
+        "llm_as_a_judge",
     ],
     "jdocqa": [
-        "yesno_exact", "factoid_exact", "numerical_exact", "open-ended_bleu", "overall"
+        "yesno_exact",
+        "factoid_exact",
+        "numerical_exact",
+        "open-ended_bleu",
+        "overall",
     ],
     "ja-multi-image-vqa": [
-        "rougel", "llm_as_a_judge",
+        "rougel",
+        "llm_as_a_judge",
     ],
     "jmmmu": [
-        "Overall-Art and Psychology", "Design", "Music", "Psychology",
-        "Overall-Business", "Accounting", "Economics", "Finance", "Manage", "Marketing",
-        "Overall-Science", "Biology", "Chemistry", "Math", "Physics",
-        "Overall-Health and Medicine", "Basic_Medical_Science", "Clinical_Medicine",
-        "Diagnostics_and_Laboratory_Medicine", "Pharmacy", "Public_Health",
-        "Overall-Tech and Engineering", "Agriculture", "Architecture_and_Engineering",
-        "Computer_Science", "Electronics", "Energy_and_Power", "Materials",
-        "Mechanical_Engineering", "Overall",
+        "Overall-Art and Psychology",
+        "Design",
+        "Music",
+        "Psychology",
+        "Overall-Business",
+        "Accounting",
+        "Economics",
+        "Finance",
+        "Manage",
+        "Marketing",
+        "Overall-Science",
+        "Biology",
+        "Chemistry",
+        "Math",
+        "Physics",
+        "Overall-Health and Medicine",
+        "Basic_Medical_Science",
+        "Clinical_Medicine",
+        "Diagnostics_and_Laboratory_Medicine",
+        "Pharmacy",
+        "Public_Health",
+        "Overall-Tech and Engineering",
+        "Agriculture",
+        "Architecture_and_Engineering",
+        "Computer_Science",
+        "Electronics",
+        "Energy_and_Power",
+        "Materials",
+        "Mechanical_Engineering",
+        "Overall",
     ],
-    "jic_vqa": [
-        "jafacility20", "jaflower30", "jafood101", "jalandmark10", "average"
-    ],
+    "jic_vqa": ["jafacility20", "jaflower30", "jafood101", "jalandmark10", "average"],
     "mecha-ja": [
-        "overall", "Factoid", "Non-Factoid", "with_background", "without_background",
+        "overall",
+        "Factoid",
+        "Non-Factoid",
+        "with_background",
+        "without_background",
     ],
     "llava-bench-in-the-wild": [
-        "rougel", "llm_as_a_judge",
+        "rougel",
+        "llm_as_a_judge",
     ],
     "mmmu": [
-        "Overall-Art and Design", "Art", "Art_Theory", "Design", "Music",
-        "Overall-Business", "Accounting", "Economics", "Finance", "Manage", "Marketing",
-        "Overall-Science", "Biology", "Chemistry", "Geography", "Math", "Physics",
-        "Overall-Health and Medicine", "Basic_Medical_Science", "Clinical_Medicine",
-        "Diagnostics_and_Laboratory_Medicine", "Pharmacy", "Public_Health",
-        "Overall-Humanities and Social Science", "History", "Literature", "Sociology", "Psychology",
-        "Overall-Tech and Engineering", "Agriculture", "Architecture_and_Engineering",
-        "Computer_Science", "Electronics", "Energy_and_Power", "Materials",
-        "Mechanical_Engineering", "Overall",
+        "Overall-Art and Design",
+        "Art",
+        "Art_Theory",
+        "Design",
+        "Music",
+        "Overall-Business",
+        "Accounting",
+        "Economics",
+        "Finance",
+        "Manage",
+        "Marketing",
+        "Overall-Science",
+        "Biology",
+        "Chemistry",
+        "Geography",
+        "Math",
+        "Physics",
+        "Overall-Health and Medicine",
+        "Basic_Medical_Science",
+        "Clinical_Medicine",
+        "Diagnostics_and_Laboratory_Medicine",
+        "Pharmacy",
+        "Public_Health",
+        "Overall-Humanities and Social Science",
+        "History",
+        "Literature",
+        "Sociology",
+        "Psychology",
+        "Overall-Tech and Engineering",
+        "Agriculture",
+        "Architecture_and_Engineering",
+        "Computer_Science",
+        "Electronics",
+        "Energy_and_Power",
+        "Materials",
+        "Mechanical_Engineering",
+        "Overall",
     ],
 }
 
@@ -90,6 +156,7 @@ METRIC_ALIAS = {
     "mmmu-Overall": "Acc",
 }
 
+
 def read_evaluation_jsonl(file_path: str) -> Dict:
     merged_result = {}
     with open(file_path, "r") as f:
@@ -97,6 +164,7 @@ def read_evaluation_jsonl(file_path: str) -> Dict:
             data = json.loads(line)
             merged_result.update(data)
     return merged_result
+
 
 def main(result_dir: str, model_list: List[str], output_path: Optional[str] = None):
     task_dirs = [d for d in os.listdir(result_dir) if not d.startswith(".")]
@@ -138,7 +206,9 @@ def main(result_dir: str, model_list: List[str], output_path: Optional[str] = No
             print(f"{task}-{metric}")
             if f"{task}-{metric}" in df.columns:
                 print(f"{metric}: {df[f'{task}-{metric}'].mean()}")
-                main_df[f"{TASK_ALIAS[task]}-{METRIC_ALIAS[metric]}"] = df[f"{task}-{metric}"]
+                main_df[f"{TASK_ALIAS[task]}-{METRIC_ALIAS[metric]}"] = df[
+                    f"{task}-{metric}"
+                ]
             else:
                 main_df[f"{TASK_ALIAS[task]}-{METRIC_ALIAS[metric]}"] = None
 
@@ -146,13 +216,14 @@ def main(result_dir: str, model_list: List[str], output_path: Optional[str] = No
     print(main_df.to_markdown(mode="github", index=False))
 
     if output_path:
-        main_df.to_json(output_path, orient="records", lines=True)
+        with open(output_path, "w") as f:
+            f.write(main_df.to_markdown(mode="github", index=False))
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--result_dir", type=str, default="paper")
-    parser.add_argument("--output_path", type=str, default="benchmark_results.json")
+    parser.add_argument("--output_path", type=str, default="leaderboard.md")
     args = parser.parse_args()
 
     # モデルは実行時引数でも取れるようにしても良い
@@ -160,7 +231,7 @@ if __name__ == "__main__":
         "Qwen/Qwen2.5-VL-7B-Instruct",
         "sbintuitions/sarashina2-vision-8b",
         "sbintuitions/sarashina2-vision-14b",
-        "google/gemma-3-12b-it"
+        "google/gemma-3-12b-it",
     ]
 
     main(args.result_dir, model_list, args.output_path)
